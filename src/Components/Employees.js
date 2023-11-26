ca3373fba79531bef9e8dfa2a749b2677d46533f
Aswin Satheesh <you@example.com>
2023-11-27 02:59:29 +0530

    crud

---
+++ src/Components/Employees.js
@@ -0,0 +1,19 @@
+const Employees =[
+{
+    id:"1",
+    Name:"Aswin Satheesh",
+    Age:"26"
+},
+{
+    id:"2",
+    Name:"Amal Unni",
+    Age:"24"
+},
+{
+    id:"3",
+    Name:"Arun Raman",
+    Age:"27"
+}
+]
+
+export default Employees;
